ddf122bee56eb820ed6dbfefc9fba6b2cec893a5
Sima <sima_tatchenkova@mail.ru>
2021-11-17 19:47:35 +0300

    hope last attempt

--- ReverseTheWords/ViewController.swift
+++ ReverseTheWords/ViewController.swift
@@ -11,9 +11,50 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var wordLabelResult: UILabel!
+    @IBOutlet weak var enterWordTextField: UITextField!
+    
+    var wordInfo = ""
+    var reversedWord = ""
+    var isClear = false
+    
+    @IBAction func reverseAction(_ sender: UIButton) {
+        if isClear == false {
+            reverseTheWord()
+            sender.setTitle("Clear", for: .normal)
+            isClear = true
+        } else if isClear {
+            clear()
+            sender.setTitle("Reverse", for: .normal)
+            isClear = false
+        }
     }
 
-
+    func reverseTheWord() {
+        wordInfo = enterWordTextField.text!
+        let parts = wordInfo.components(separatedBy: " ")
+        let reversed = parts.map{String($0.reversed())}
+        reversedWord = reversed.joined(separator: " ")
+        printOutTheWord()
+        hideTheKeyBoard()
+    }
+        
+    func clear() {
+        enterWordTextField.text = ""
+        wordLabelResult.text = ""
+        hideTheKeyBoard()
+    }
+    func printOutTheWord() {
+        wordLabelResult.text = "\(reversedWord)"
+    }
+    func hideTheKeyBoard() {
+        enterWordTextField.resignFirstResponder()
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        hideTheKeyBoard()
+    }
 }
+
 
